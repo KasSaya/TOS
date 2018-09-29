d49f0aaddf89bbc4acc5ff57cdd20f9d9a3fc562
刘超 <liuchao@aixuedai.com>
2018-09-29 16:38:11 +0800

    add files

---
+++ static/js/rem.js
@@ -0,0 +1,9 @@
+(function () {
+    var html = document.documentElement;
+    var hWidth = window.screen.width;
+    html.style.fontSize = ((hWidth / 15).toFixed(2)) + "px";
+})();
+//var baseUrl='http://192.168.8.108:8023';
+
+// var baseUrl ='http://192.168.8.142:8023';
+var baseUrl='http://111.1.17.82:8023';
